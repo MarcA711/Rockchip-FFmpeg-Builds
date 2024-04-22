bcbd7a287205420b713500a492bf450956d6bc9b
MarcA711 <40744649+MarcA711@users.noreply.github.com>
2024-04-22 21:09:19 +0200

    use nyanmisaka's fork of mpp and delete shared libs to avoid build errors

--- scripts.d/50-rkmpp.sh
+++ scripts.d/50-rkmpp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/rockchip-linux/mpp.git"
-SCRIPT_COMMIT="bebc9961103af2b53fb18175dd858b15a73c9ad8"
+# SCRIPT_REPO="https://github.com/rockchip-linux/mpp.git"
+# SCRIPT_COMMIT="bebc9961103af2b53fb18175dd858b15a73c9ad8"
+
+SCRIPT_REPO="https://github.com/nyanmisaka/mpp.git"
+SCRIPT_COMMIT="42070d0658fa8ccdd7b0726a46edc518fd908e02"
+SCRIPT_BRANCH="jellyfin-mpp"
 
 ffbuild_enabled() {
     [[ $ADDINS_STR == *-rk ]] && return 0
@@ -15,6 +19,8 @@ ffbuild_dockerbuild() {
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DBUILD_TEST=OFF ..
     make -j$(nproc)
     make install
+
+    rm -rf $FFBUILD_PREFIX/lib/librockchip_mpp.so*
 }
 
 ffbuild_configure() {
